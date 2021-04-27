e9702878edfc9a35d04ef49f9cf6f3e7416853b8
EvanVeinotte <44095792+EvanVeinotte@users.noreply.github.com>
2021-04-27 16:00:21 -0300

    Updated github repository to be completed version

---
+++ build/precache-manifest.43030a3dd88690390f6e23dae59219a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e4116554bc0385fe16ccb36c5046c2",
+    "url": "/place/index.html"
+  },
+  {
+    "revision": "abf9be8bfdbe5543eb1b",
+    "url": "/place/static/css/main.cfdbe6cf.chunk.css"
+  },
+  {
+    "revision": "55efcab30f95b144c0da",
+    "url": "/place/static/js/2.8e0e564b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/place/static/js/2.8e0e564b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf9be8bfdbe5543eb1b",
+    "url": "/place/static/js/main.3cee45f7.chunk.js"
+  },
+  {
+    "revision": "9cd7fed4669f8503b5d8",
+    "url": "/place/static/js/runtime-main.967e7dbe.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const WIDTH = 32;
+const HEIGHT = 32;
+
+class Pixel extends React.Component {
+  render(){
+    return(
+      <div className='pixel' style={{backgroundColor: this.props.backgroundColor}}>
+
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      pixels: Array(WIDTH).fill(Array(HEIGHT).fill('#000000'))
+    };
+  }
+
+  fetchData(){
+    fetch('http://vino0244.xyz/place/data')
+    .then(res => res.json())
+    .then(json => this.syncPixels(json, this));
+  
+    setTimeout(this.fetchData.bind(this), 3000);
+  }
+
+  syncPixels(data){
+    //console.log('syncing');
+    this.setState({pixels: data.matrix});
+  }
+
+  renderPixel(x, y){
+    return <Pixel key={x.toString() + y.toString()} backgroundColor={this.state.pixels[x][y]}/>
+  }
+
+  renderRow(x){
+    let pixel_array = [];
+    for (let i=0; i<WIDTH; i++){
+      pixel_array.push(this.renderPixel(x, i));
+    }
+
+    return <div className="row" key={x}>{pixel_array}</div>;
+  }
+
+  renderBoard(){
+    let pixel_matrix = [];
+    for (let i=0; i<HEIGHT; i++){
+      pixel_matrix.push(this.renderRow(i));
+    }
+
+    return pixel_matrix;
+  }
+
+  componentDidMount(){
+    this.fetchData();
+  }
+
+  render(){
+    return (
+      <div className="board-container">
+        <div className="theboard">
+          {this.renderBoard()}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
+class Page extends React.Component{
+  render(){
+    return(
+      <div>
+        <div className="title-txt">Visit&nbsp;<a href="https://twitch.tv/vino0244/">https://twitch.tv/vino0244/</a>&nbsp;to participate!</div>
+        <Board />
+      </div>
+    );
+  }
+}
+
+ReactDom.render(
+  <Page />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
